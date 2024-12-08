85b706a3a353e73b0dc08afee0d56e99fe007266
c-pupil <56547062+c-pupil@users.noreply.github.com>
2024-12-08 19:15:17 +0800

    commit

--- datasets/lu.py
+++ datasets/lu.py
@@ -15,31 +15,51 @@ class LU_dataset(Dataset):
         self.scale = scale
         self.image_files = sorted(glob.glob(os.path.join(root_dir, "RGB", "*.png")))
         self.depth_files = sorted(glob.glob(os.path.join(root_dir, "Depth", "*.png")))
+        self.mde_files = sorted(glob.glob(os.path.join(root_dir, "MDE_relative", "*.png")))#add
         self.size = len(self.image_files)
 
     def __getitem__(self, idx):
         image_file = self.image_files[idx]
         depth_file = self.depth_files[idx]
+        mde_file = self.mde_files[idx] #add
         
         image = cv2.imread(image_file).astype(np.float32)   # [H, W, 3]
         depth = cv2.imread(depth_file)[:,:,0].astype(np.float32) # [H, W]
-        
+        mde=cv2.imread(mde_file)[:,:,0].astype(np.float32) # [H, W]
+        mde=mde.max()-mde
+
         h, w = depth.shape
         s = self.scale
         target = np.array(Image.fromarray(depth).resize((w//s,h//s),Image.BICUBIC))
+
+        noise = np.random.normal(0, 5, target.shape)
+        gaussian_out = target + noise
+        gaussian_out = np.clip(gaussian_out, 0, 255)
+
         # normalize
         image = image.transpose(2,0,1) / 255.
+
         depth_min = depth.min()
         depth_max = depth.max()
         target = (target - depth_min) / (depth_max - depth_min)
         depth = (depth - depth_min) / (depth_max - depth_min)
+        gaussian_out = (gaussian_out - depth_min) / (depth_max - depth_min) #
+
+
+
+        mde_min = mde.min()
+        mde_max = mde.max()
+        mde = (mde - mde_min) / (mde_max - mde_min)
+        
         # to tensor
         image = torch.from_numpy(image).float()
         depth = torch.from_numpy(depth).unsqueeze(0).float()
+        mde = torch.from_numpy(mde).unsqueeze(0).float()
         target = torch.from_numpy(target).unsqueeze(0).float()
+        gaussian_out = torch.from_numpy(gaussian_out).unsqueeze(0).float()
 
         sample = {'guidance': image, 'target': target, 'gt': depth, 'min': depth_min,
-                'max': depth_max}
+                'max': depth_max,'mde':mde,'noise_target':gaussian_out}
     
         return sample
 
--- option.py
+++ option.py
@@ -9,15 +9,14 @@ parser.add_argument('--model_name', type=str, default='D2A2',
                     help='choose model class name')
 parser.add_argument('--model_file', type=str, default='D2A2', 
                     help='choose model file')
-parser.add_argument('--scale', type=int, default=4, 
+parser.add_argument('--scale', type=int, default=16,##4 | 8 | 16
                     help='scale factor')
-parser.add_argument('--dataset', type=str, default='nyu',
+parser.add_argument('--dataset', type=str, default='lu',
                     help='dataset nyu, middlebury, lu or rgbdd')  
-parser.add_argument('--dataset_dir', type=str, default='',
-                    help='dataset root dir')   
+parser.add_argument('--dataset_dir', type=str, default='/home/zyl/D2A2/load/Lu',#Lu | Middlebury | RGBDD | NYUv2
+                    help='dataset root dir')
 parser.add_argument('--pretrain_path', type=str, default=None,
-                    help='load pretrained model parameters')                  
-
+                    help='load pretrained model parameters')
 # Model setting
 parser.add_argument('--n_resblocks', type=int, default=4,
                     help='The number of residual blocks in each stage')
@@ -27,11 +26,12 @@ parser.add_argument('--res_scale', type=float, default=1.,
                     help='Residual scale')
 
 # Train setting
-parser.add_argument('--input_size', type=int, default=256, 
+parser.add_argument('--input_size', type=int, default=256,
                     help='input size')
-parser.add_argument('--epoch', type=int, default=500, 
+parser.add_argument('--epoch', type=int, default=600,
                     help='max epoch')
-parser.add_argument('--batch_size', type=int, default=4, 
+
+parser.add_argument('--batch_size', type=int, default=4,
                     help='batch size')
 parser.add_argument('--augment', type=bool, default=True,
                     help='data augment')
@@ -39,20 +39,26 @@ parser.add_argument('--lr', type=float, default='0.0001',
                     help='learning rate')
 parser.add_argument('--step_size', type=int, default=5000000, 
                     help='learning rate step size')
-parser.add_argument('--trainresult', type=str, default='./result/trainresult/', 
+parser.add_argument('--trainresult', type=str, default='./result/trainresult/',
                     help=' train result file path')
 parser.add_argument('--last_epoch', type=int, default=-1,
                     help='train epoch')
+parser.add_argument('--loss', type=str, default='maskloss', #l1 | our loss
+                    help='choice loss')
 
 # Test setting 
-parser.add_argument('--save', action='store_true',
+parser.add_argument('--save', action='store_true',#save results
                     help='save depth SR or not') 
 parser.add_argument('--testresult', type=str, default='./result/testresult/', 
                     help=' train result file path')
 parser.add_argument('--net_path', type=str, default=None,
-                    help='load pretrained model parameters') 
+                    help='load pretrained model parameters') #put model parameters
 
 
+
+
+#depthanything encoder
+parser.add_argument('--encoder', type=str, default='vitb', choices=['vits', 'vitb', 'vitl', 'vitg'])
 
 args = parser.parse_args()
 # print(args)
